d79035121b6661ad70aaea60e30628112f86a625
Souleye <sulaynd@gmail.com>
2026-01-02 15:46:35 -0500

    adding WithMockUser on tests

--- bookstore-webapp/src/test/java/com/louly/soft/webapp/BookstoreWebappApplicationTests.java
+++ bookstore-webapp/src/test/java/com/louly/soft/webapp/BookstoreWebappApplicationTests.java
@@ -1,10 +1,13 @@
 package com.louly.soft.webapp;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 
 @SpringBootTest
 class BookstoreWebappApplicationTests {
 
-    // @Test
+    @Test
+    @WithMockUser
     void contextLoads() {}
 }
