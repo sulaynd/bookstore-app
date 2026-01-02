77f7c55c38871166b31c47e2c762806e7e553a5a
Souleye <sulaynd@gmail.com>
2026-01-02 15:41:23 -0500

    adding bookstore-app tests

---
+++ bookstore-webapp/src/test/java/com/louly/soft/webapp/BookstoreWebappApplicationTests.java
@@ -0,0 +1,11 @@
+package com.louly.soft.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreWebappApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
